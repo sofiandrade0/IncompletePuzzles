5bb73879ee67b1a2cbddfc7865d013cb84e42fee
Russell Gordon <rgordon@lcs.on.ca>
2020-09-30 21:58:53 -0400

    Added partial solution to the Cell Sell puzzle.

--- TheCellSell/main.swift
+++ TheCellSell/main.swift
@@ -12,3 +12,44 @@ import Foundation
 
 print("The Cell Sell")
 print("=============")
+
+// INPUT
+
+// Get daytime minutes
+print("Number of daytime minutes?")
+let day = Int(readLine()!)!
+
+// Get evening minutes
+print("Number of evening minutes?")
+let evening = Int(readLine()!)!
+
+// PROCESS
+
+// Calculate cost for plan A
+var a = 0
+
+// Add daytime cost
+a += (day - 100) * 25
+
+// Add evening cost
+a += evening * 15
+
+// Calculate cost for plan B
+var b = 0
+
+// Add daytime cost
+b += (day - 250) * 45
+
+// Add evening cost
+b += evening * 35
+
+
+// OUTPUT
+print("Plan A costs \(a)")
+print("Plan B costs \(b)")
+
+if a > b {
+    print("Plan B is cheapest.")
+} else {
+    print("Plan A is cheapest.")
+}
