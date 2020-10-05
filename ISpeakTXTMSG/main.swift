7a0b3d63bfc58481007b5c3d41243e7631e49160
Russell Gordon <rgordon@lcs.on.ca>
2020-10-05 10:35:19 -0400

    Start of the "I speak TXTMSG" puzzle.

---
+++ ISpeakTXTMSG/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  I Speak TXTMSG
+//
+//  Puzzle description available at:
+//
+//  https://www.russellgordon.ca/incomplete-puzzles/i-speak-txtmsg.pdf
+//
+//  NOTE: Puzzle adapted from material provided by the University of Waterloo.
+
+import Foundation
+
+print("I Speak TXTMSG")
+print("==============")
+
+// INPUT
+
+// Ask for a phrase
+print("Enter phrase> ", terminator: "")
+let phrase = readLine()!
+
+// PROCESS AND OUTPUT
+// NOTE: Instead of an "if statement" using a different type of Swift structure to handle all the different possible cases...
+if phrase == "TA" {
+    print("totally awesome")
+} else if phrase == "TTYL" {
+    print("talk to you later")
+    exit(0)
+}
