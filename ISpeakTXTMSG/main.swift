bddcdefa1e44d6585afda5d25d927a7e935794bb
Sofia Andrade <sandrade@lcs.on.ca>
2020-10-15 08:05:30 -0500

    I added a loop, so the phrases keep going -  TXTMSG

--- ISpeakTXTMSG/main.swift
+++ ISpeakTXTMSG/main.swift
@@ -15,45 +15,53 @@ print("==============")
 
 // INPUT
 
-// Ask for a phrase
-print("Enter phrase> ", terminator: "")
-let phrase = readLine()!
+// Ask for a phrase, repeatedly
+while true {
+    
+    print("Enter phrase> ", terminator: "")
+    let phrase = readLine()!
+
+
+    // NOTE: Instead of an "if statement" using a different type of Swift structure to handle all the different possible cases...
+    if phrase == "TA" {
+        print("totally awesome")
+    } else if phrase == "TTYL" {
+        print("talk to you later")
+        exit(0)
+    }
+    if phrase == "CU" {
+        print("See you")
+    } else if phrase == ":-)" {
+        print("I'm happy")
+        exit(0)
+    }
+    if phrase == ":-(" {
+        print("I'm unhappy")
+    } else if phrase == ";-)" {
+        print("Wink")
+        exit(0)
+    }
+    if phrase == ":-P" {
+        print("Stick out my tongue")
+    } else if phrase == "(˜.˜)" {
+        print("sleepy")
+        exit(0)
+    }
+    if phrase == "CCC" {
+        print("Canadian Computing Competition")
+    } else if phrase == "CUZ" {
+        print("Because")
+        exit(0)
+    }
+    if phrase == "TY" {
+        print("Thank-you")
+    } else if phrase == "YW" {
+        print("You’re welcome")
+        exit(0)
+    }
+    
+}
+
 
 // PROCESS AND OUTPUT
-// NOTE: Instead of an "if statement" using a different type of Swift structure to handle all the different possible cases...
-if phrase == "TA" {
-    print("totally awesome")
-} else if phrase == "TTYL" {
-    print("talk to you later")
-    exit(0)
-}
-if phrase == "CU" {
-    print("See you")
-} else if phrase == ":-)" {
-    print("I'm happy")
-    exit(0)
-}
-if phrase == ":-(" {
-    print("I'm unhappy")
-} else if phrase == ";-)" {
-    print("Wink")
-    exit(0)
-}
-if phrase == ":-P" {
-    print("Stick out my tongue")
-} else if phrase == "(˜.˜)" {
-    print("sleepy")
-    exit(0)
-}
-if phrase == "CCC" {
-    print("Canadian Computing Competition")
-} else if phrase == "CUZ" {
-    print("Because")
-    exit(0)
-}
-if phrase == "TY" {
-    print("Thank-you")
-} else if phrase == "YW" {
-    print("You’re welcome")
-    exit(0)
-}
+
